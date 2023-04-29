2c798bb3da5586a82ec88a7bc80a31447e621c58
wighawag <wighawag@gmail.com>
2023-04-29 21:00:27 +0100

    test call

--- examples/demo-js/example.ts
+++ examples/demo-js/example.ts
@@ -1,13 +1,18 @@
-//
 import { execute } from "forge-exec-ipc-server";
-import { encodeDeployData } from 'viem';
+import { encodeDeployData, encodeFunctionData } from 'viem';
 
 import Counter from './contracts/Counter.sol:Counter.js';
 
 execute(async (forge) => {
   const counter = await forge.create({
     from: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-    data:encodeDeployData({abi: Counter.abi, bytecode: Counter.bytecode })
+    data:encodeDeployData(Counter)
+  });
+
+  await forge.call({
+    from: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+    to: counter,
+    data: encodeFunctionData({...Counter, functionName: 'setNumber', args: [42n]})
   });
 
   return {
--- examples/demo-js/test/Exec.t.sol
+++ examples/demo-js/test/Exec.t.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 import {Exec} from "forge-exec/Exec.sol";
+import {Counter} from 'src/Counter.sol';
 
 address constant FirstContract = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
 
 contract ExecTest is Test {
+    Counter counter;
     function setUp() public {
         string[] memory args = new string[](1);
         args[0] = "./example.js";
@@ -17,29 +19,32 @@ contract ExecTest is Test {
             (address, address, address)
         );
 
+        counter = Counter(addr1);
+
         console.log("----");
         console.log(addr1, addr2, addr3);
         console.logBytes(test);
         console.log("----");
     }
 
-    function testDeplyment() public view {
+    function testDeplyment() public {
         bytes memory code = FirstContract.code;
         assert(code.length > 0);
+        assertEq(counter.number(), 42);
     }
 
-    function testDeplymentAgain() public view {
+    function testDeplymentAgain() public  {
         bytes memory code = FirstContract.code;
-        assert(code.length > 0);
+        assertGt(code.length, 0);
     }
 
-    function testDeplymentAgainAndAgain() public view {
+    function testDeplymentAgainAndAgain() public {
         bytes memory code = FirstContract.code;
-        assert(code.length > 0);
+        assertGt(code.length, 0);
     }
 
-    function testDeplymentAgainAndAgainAndAgain() public view {
+    function testDeplymentAgainAndAgainAndAgain() public {
         bytes memory code = FirstContract.code;
-        assert(code.length > 0);
+        assertGt(code.length, 0);
     }
 }
